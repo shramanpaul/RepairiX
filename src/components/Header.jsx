e14e11cbf56de66be4a6c538a59a246cea78ff9c
shraman <22bcs229@iiitdmj.ac.in>
2023-08-14 01:03:56 +0530

    react router set between header and App

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Box, Button } from '@chakra-ui/react';
+function Header() {
+    const [count, setCount] = useState(0)
+  
+    return (
+      <>
+        
+        <Box p={4}>
+        <Button colorScheme="blue">Chakra Button</Button>
+      </Box>
+   
+      </>
+    )
+  }
+  export default Header;
