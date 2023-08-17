3405dc6a2678ac2e7c0386d3bf2056ef23a5cdcf
Pratik <pratiksolanki2026@gmail.com>
2023-08-18 03:33:37 +0530

    feat: Add Navbar Component

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,20 +1,48 @@
 import { useState } from "react";
-import { Box, Button } from "@chakra-ui/react";
+import logo from "../assets/logo.png";
+import {
+  Heading,
+  Flex,
+  Box,
+  Button,
+  UnorderedList,
+  ListItem,
+  Image,
+} from "@chakra-ui/react";
 import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
-import { Checkbox, CheckboxGroup } from '@chakra-ui/react';
-function Header() {
-  const [count, setCount] = useState(0);
+import { Checkbox, CheckboxGroup } from "@chakra-ui/react";
 
+function Navbar() {
   return (
     <>
-      <Box p={4}>
-        <Button colorScheme="blue">Chakra Button</Button>
-      </Box>
-      <Box as="button" bg="tomato" w="20%" p={4} color="white">
-        This is the Box
-      </Box>
-      
+      <Flex
+        w="100vw"
+        h="12vh"
+        bgColor="#f7f8fc"
+        justify="space-between"
+        alignItems="center"
+        pos="fixed"
+        boxShadow="rgba(157, 157, 157, 0.2) 0px 4px 10px"
+      >
+        <Image src={logo} pl={{ base: 2, md: 6, lg: 10 }}></Image>
+
+        <UnorderedList
+          display="flex"
+          listStyleType="none"
+          gap={{ base: 2, md: 6, lg: 8 }}
+          fontFamily="sans-serif"
+          fontSize="lg"
+          fontWeight="medium"
+        >
+          <ListItem _hover={{ color: "#6674CC" }}>Home</ListItem>
+          <ListItem _hover={{ color: "#6674CC" }}>Services</ListItem>
+          <ListItem _hover={{ color: "#6674CC" }}>About us</ListItem>
+          <ListItem _hover={{ color: "#6674CC" }}>Contact</ListItem>
+        </UnorderedList>
+
+        <Heading pr={{ base: 2, md: 6, lg: 10 }}>Image</Heading>
+      </Flex>
     </>
   );
 }
-export default Header;
+export default Navbar;
